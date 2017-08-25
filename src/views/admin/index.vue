713c3079648e42dce1e9446e1e4e992634e84510
BroccoliSpring <Lensgcx@163.com>
2017-08-25 21:40:31 +0800

    admin index change
    admin index change

--- src/views/admin/index.vue
+++ src/views/admin/index.vue
@@ -17,9 +17,9 @@
             </el-col>
         </el-row>
         <!--<code>-->
-            <!--这里的所有的图表都基于ECharts,实例代码来源-->
-            <!--<a href='http://gallery.echartsjs.com/explore.html#sort=rank~timeframe=all~author=all' target='_blank'>gallery</a><br/>其实ECharts封装的很好了，用vue封装是很简单的事情，建议大家自己来封装。<a-->
-                <!--target='_blank' class='lin' href="https://segmentfault.com/a/1190000009762198#articleHeader16">相关教程</a>-->
+        <!--这里的所有的图表都基于ECharts,实例代码来源-->
+        <!--<a href='http://gallery.echartsjs.com/explore.html#sort=rank~timeframe=all~author=all' target='_blank'>gallery</a><br/>其实ECharts封装的很好了，用vue封装是很简单的事情，建议大家自己来封装。<a-->
+        <!--target='_blank' class='lin' href="https://segmentfault.com/a/1190000009762198#articleHeader16">相关教程</a>-->
         <!--</code>-->
     </div>
 </template>
@@ -36,6 +36,16 @@
             }
         },
         created () {
+            console.log(33333333);
+            this.axios.post(this.$store.state.mock.url.userListDataUrl, {}, data => {
+                },
+                r => {
+                    console.log(22222222222);
+                    console.log(r);
+                }
+            );
+
+
             //加载 圆饼图数据
             this.pieChart(this, this.$store.state.mock.url.pieChart_staffUrl, {'token': this.$store.state.user.info.token, 'command': 'staff'},
                 () => {
